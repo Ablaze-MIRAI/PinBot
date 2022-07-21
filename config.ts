bcde368285f6c9bbf77384d563cfb4b28aeed9bc
Raisan <ytpc26ms@outlook.jp>
2022-07-22 00:41:24 +0900

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,5 +2,5 @@ export default {
     TOKEN: process.env.DISCORD_API_TOKEN,
     DEBUG: undefined,
     VERSION: "v1.0.0",
-    GITHUB: "https://github.com/"
+    GITHUB: "https://github.com/Ablaze-MIRAI/PinBot/"
 }
